b301d1b70f593d75c91bafeb546fcae607ac6e7d
Iara Rodrigues <loja@chivafit.com.br>
2026-08-24 20:51:34 -0300

    Adiciona favicon oficial do Guia Saúde

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,7 @@ export const metadata: Metadata = {
   description: defaultDescription,
   icons: {
     icon: [{ url: "/favicon.svg", type: "image/svg+xml" }, { url: "/favicon-32.png", sizes: "32x32", type: "image/png" }],
+    shortcut: "/favicon.svg",
     apple: [{ url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" }],
   },
   applicationName: siteName,
