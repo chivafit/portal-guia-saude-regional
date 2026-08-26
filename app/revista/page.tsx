56f507e8488b369811bba84cbdc6f93c5fb200c2
Iara Rodrigues <loja@chivafit.com.br>
2026-08-26 19:51:57 -0300

    Corrige contraste da chamada comercial da revista

--- app/revista/page.tsx
+++ app/revista/page.tsx
@@ -3,5 +3,6 @@ import { SiteFooter } from "@/components/SiteFooter";
 import { MagazineCatalog } from "@/components/MagazineCatalog";
 import { magazineEditions } from "@/lib/data";
 import { pageMetadata } from "@/lib/seo";
+import "./revista.css";
 export const metadata = pageMetadata("Revista digital", "Edições da Revista Guia Saúde com histórias, especialistas, entrevistas e conteúdo regional de saúde.", "/revista");
 export default function RevistaPage(){return <><SiteHeader advertiseLabel="Anuncie no Guia"/><MagazineCatalog editions={magazineEditions}/><SiteFooter/></>}
